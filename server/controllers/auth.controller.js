75f8aef7e4ede7f5bbf9f925a1d2eecfda488867
Noor <thenoorulhassan.dev@gmail.com>
2026-03-19 22:53:06 +0500

    Login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -66,5 +66,18 @@ export const login = async (req, res) => {
         message: "Incorrect Password",
       });
     }
-  } catch (error) {}
+
+    generateWebToken(user, res);
+    return res.status(200).json({
+      success: true,
+      message: "Login Successfull",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Login",
+    });
+  }
 };
